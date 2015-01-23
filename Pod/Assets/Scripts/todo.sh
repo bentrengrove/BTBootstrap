a0342caea6bc966ab493e3476af4db2244c39dc4
Ben Trengrove <b.trengrove@gmail.com>
2015-01-23 12:26:19 +1100

    Add swift to TODO checking

--- Pod/Assets/Scripts/todo.sh
+++ Pod/Assets/Scripts/todo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 KEYWORDS="TODO:|VERIFY:|FIXME:|\?\?\?:|\!\!\!:"
-find "${SRCROOT}" -not -path "${SRCROOT}/Pods/*" \( -name "*.h" -or -name "*.m" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/ warning: \$1/"
+find "${SRCROOT}" -not -path "${SRCROOT}/Pods/*" \( -name "*.h" -or -name "*.m" -or -name "*.swift"\) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/ warning: \$1/"
